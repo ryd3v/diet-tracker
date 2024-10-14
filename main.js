9dc674afa6affa12fcc9edc217ad6e29339f3fb3
ryd3v <hello@ryd3v.com>
2024-10-14 02:52:20 -0400

    add icons

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+const iconPath = process.platform === 'win32'
+    ? path.join(__dirname, 'assets', 'icon.ico')
+    : path.join(__dirname, 'assets', 'icon.png');
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: iconPath, // Use iconPath here
+    });
 
-  //win.loadFile('index.html');
-  //win.loadURL(`file://${__dirname}/build/index.html`);
-  win.loadURL(`http://localhost:3000`);
+    // Load your app
+    win.loadURL(`http://localhost:3000`);
 
-  win.on('closed', () => {
-    win = null;
-  });
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
+    if (win === null) {
+        createWindow();
+    }
 });
